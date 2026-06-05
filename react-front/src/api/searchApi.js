bffc883b10c97e2f3b81517dfddabf2d8be6473c
Medo-skb <kiryto0912@gmail.com>
2026-06-05 17:44:20 +0900

    Delete react-front directory

--- react-front/src/api/searchApi.js
+++
@@ -1,8 +0,0 @@
-import { apiRequest } from './apiClient';
-
-export function getSearchSuggestions({ keyword }) {
-  return apiRequest('/search/suggestions', {
-    auth: true,
-    query: { keyword },
-  });
-}
